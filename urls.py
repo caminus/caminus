81f83c54ec372083d35d4193fb7a14940a62a690
Trever Fischer <tdfischer@fedoraproject.org>
2012-02-19 17:49:01 -0500

    Cleanup urls.py and add api

--- urls.py
+++ urls.py
@@ -1,24 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^profiles/', include('profiles.urls')),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/profile/$', 'profiles.views.profile'),
-    # Examples:
     url(r'^$', 'profiles.views.profile', name='home'),
-    # url(r'^caminus/', include('caminus.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^profiles/', include('profiles.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^forums/', include('forums.urls'))
+    url(r'^forums/', include('forums.urls')),
+    url(r'^api/', include('api.urls'))
 )
 
 urlpatterns += staticfiles_urlpatterns()
