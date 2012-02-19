4b8d75ab5e51a4a9522163ba6652556431317d8e
Trever Fischer <tdfischer@fedoraproject.org>
2012-02-19 17:51:49 -0500

    Expose minecraft host to templates

--- minecraft/context.py
+++ minecraft/context.py
@@ -4,4 +4,4 @@ from django.conf import settings
 def server_info(request):
     client = pyspy.GamespyClient(settings.MINECRAFT_SERVER, settings.MINECRAFT_SERVER_PORT)
     client.update()
-    return {'onlinePlayers':client.players()}
+    return {'onlinePlayers':client.players(), 'minecraftHost': settings.MINECRAFT_SERVER, 'minecraftPort': settings.MINECRAFT_SERVER_PORT}
