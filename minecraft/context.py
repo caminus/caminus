311c26e44510ed9a0fb50a94fbcbff349c8992df
Trever Fischer <tdfischer@fedoraproject.org>
2012-02-19 22:28:27 -0500

    Cache gamespy queries

--- minecraft/context.py
+++ minecraft/context.py
@@ -1,7 +1,12 @@
 import pyspy
 from django.conf import settings
+from django.core.cache import cache
 
 def server_info(request):
-    client = pyspy.GamespyClient(settings.MINECRAFT_SERVER, settings.MINECRAFT_SERVER_PORT)
-    client.update()
-    return {'onlinePlayers':client.players(), 'minecraftHost': settings.MINECRAFT_SERVER, 'minecraftPort': settings.MINECRAFT_SERVER_PORT}
+    players = cache.get('minecraftPlayerList')
+    if players is None:
+        client = pyspy.GamespyClient(settings.MINECRAFT_SERVER, settings.MINECRAFT_SERVER_PORT)
+        client.update()
+        players = client.players()
+        cache.set('minecraftPlayerList', players, 120)
+    return {'onlinePlayers':players, 'minecraftHost': settings.MINECRAFT_SERVER, 'minecraftPort': settings.MINECRAFT_SERVER_PORT}
