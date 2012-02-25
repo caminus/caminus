52429225aaa8f916ba9796ec497035f41aa5d010
Trever Fischer <tdfischer@fedoraproject.org>
2012-02-25 12:40:21 -0500

    Allow interface to specify alternate git repo path

--- appversion/context.py
+++ appversion/context.py
@@ -1,7 +1,8 @@
 import subprocess
+from django.conf import settings
 
 def git_version(request):
-    proc = subprocess.Popen(["git", "describe"], stdout=subprocess.PIPE)
+    proc = subprocess.Popen(["git", "--git-dir", settings.APPVERSION_GIT_REPO, "describe"], stdout=subprocess.PIPE)
     proc.wait()
     version = proc.stdout.read().strip()
     return {"app_version": version}
