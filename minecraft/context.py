a45a3845da2757abf754c4965abb885536dbfdc6
Trever Fischer <tdfischer@fedoraproject.org>
2012-02-27 11:52:46 -0500

    Dont crash if we can't resolve DNS

--- minecraft/context.py
+++ minecraft/context.py
@@ -5,8 +5,11 @@ from django.core.cache import cache
 def server_info(request):
     players = cache.get('minecraftPlayerList')
     if players is None:
-        client = pyspy.GamespyClient(settings.MINECRAFT_SERVER, settings.MINECRAFT_SERVER_PORT)
-        client.update()
-        players = client.players()
-        cache.set('minecraftPlayerList', players, 120)
+        try:
+            client = pyspy.GamespyClient(settings.MINECRAFT_SERVER, settings.MINECRAFT_SERVER_PORT)
+            client.update()
+            players = client.players()
+            cache.set('minecraftPlayerList', players, 120)
+        except socket.error:
+            pass
     return {'onlinePlayers':players, 'minecraftHost': settings.MINECRAFT_SERVER, 'minecraftPort': settings.MINECRAFT_SERVER_PORT}
