e7a13648526bfc86f72e5c38233da908f8655f52
Trever Fischer <tdfischer@fedoraproject.org>
2012-03-05 19:51:00 -0500

    Simplify forum URLs. Fixes #25

--- forums/urls.py
+++ forums/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('forums',
     url(r'^$', 'views.index'),
-    url(r'^forum/(?P<forum>.*)/$', 'views.forum'),
-    url(r'^forum/(?P<forumID>.*)/new$', 'views.newTopic'),
-    url(r'^forum/(?P<forumSlug>.*)/(?P<topicID>.*)/(?P<topicSlug>.*)$', 'views.topic'),
+    url(r'^(?P<forum>.*)/$', 'views.forum'),
+    url(r'^(?P<forumID>.*)/new$', 'views.newTopic'),
+    url(r'^(?P<forumSlug>.*)/(?P<topicID>.*)/(?P<topicSlug>.*)$', 'views.topic'),
     url(r'^topic/(?P<topicID>.*)$', 'views.topic'),
     url(r'^reply/(?P<topicID>.*)', 'views.reply'),
     url(r'^reply$', 'views.reply'),
