13beecf95629b055d614d3e34540f63b0a5b3f1c
Trever Fischer <tdfischer@fedoraproject.org>
2012-03-07 18:03:38 -0500

    Add invite tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,16 +1,38 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.utils import unittest
+from django.test.client import Client
+import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-Replace this with more appropriate tests for your application.
-"""
+class InviteTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('ValidUsername', 'test@example.com')
+        self.user.save()
+        self.invite = models.Invite()
+        self.invite.creator = self.user
+        self.invite.save()
 
-from django.test import TestCase
+    def tearDown(self):
+        self.invite.delete()
+        self.user.delete()
 
+    def testTryBadInvite(self):
+        resp = self.client.get(reverse('profiles.views.claimInvite', kwargs={'code':self.invite.code+"-invalid"}), follow=True)
+        self.assertEqual(404, resp.status_code)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def testTryToReuseInvite(self):
+        self.invite.claimer = self.user
+        self.invite.save()
+        resp = self.client.get(reverse('profiles.views.claimInvite', kwargs={'code':self.invite.code}), follow=True)
+        self.assertEqual(404, resp.status_code)
+
+    def testTryToUseDeletedInvite(self):
+        self.invite.deleted = True
+        self.invite.save()
+        resp = self.client.get(reverse('profiles.views.claimInvite', kwargs={'code':self.invite.code}), follow=True)
+        self.assertEqual(404, resp.status_code)
+
+    def testUseInvite(self):
+        resp = self.client.get(reverse('profiles.views.claimInvite', kwargs={'code':self.invite.code}), follow=True)
+        self.assertEqual(200, resp.status_code)
