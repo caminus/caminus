b2bd346c3565d368561d91490036a16f160efa4a
Trever Fischer <tdfischer@fedoraproject.org>
2012-04-18 11:28:46 -0400

    Remove the news app

--- news/admin.py
+++
@@ -1,5 +0,0 @@
-import models
-from django.contrib import admin
-
-admin.site.register(models.Post)
-admin.site.register(models.Comment)
--- news/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-import models
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = models.Comment
-        exclude = ('author', 'post', 'parent', 'created', 'updated')
