9d7ca78ff33c96790e4e3ff95a03663364e0726e
Trever Fischer <tdfischer@fedoraproject.org>
2012-06-07 16:59:19 -0400

    No need to check petitions if we are anonymous

--- petition/context.py
+++ petition/context.py
@@ -1,6 +1,8 @@
 import models
 
 def open_petitions(request):
+    if not request.user.is_authenticated():
+        return {}
     if request.user.is_staff or request.user.is_admin:
         petitions = models.Petition.objects.filter(closed=False)
     else:
