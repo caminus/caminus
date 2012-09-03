d18d17a97ae4df2198aeea711b831d192dd34e12
Trever Fischer <tdfischer@fedoraproject.org>
2012-09-03 18:08:09 -0400

    debug--

--- static/js/interactive.js
+++ static/js/interactive.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#server-interaction .drawer').each(function() {
-    console.log(this);
     var canvas = $(this).children('.canvas');
     $(this, '.drawer-label').click(function() {
       canvas.slideToggle("blind");
